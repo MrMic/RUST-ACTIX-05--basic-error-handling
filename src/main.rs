c1dce15a72304f37b622d76e00f552dd6fb5261f
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-11-24 14:52:45 +0100

    feat: first step

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, Error, HttpResponse, HttpServer};
+
+async fn hello() -> Result<HttpResponse, Error> {
+    Ok(HttpResponse::Ok().body("Hello there!"))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/hello", web::get().to(hello)))
+        .bind("127.0.0.1:3002")?
+        .run()
+        .await
 }
